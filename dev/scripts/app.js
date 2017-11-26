525fe672fa6ab50cc75e471f510a68f557ec3598
Gaby Kupfer <g.kupfer@hotmail.com>
2017-11-26 11:35:56 -0500

    Added transition functionality

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
-}
+const Home = () => {
+  return (
+    <div>
+      <ReactCSSTransitionGroup transitionName="anim" transitionAppear={true} transitionAppearTimeout={5000} transitionEnter={false} transitionLeave={false}>
+        <h2>{'TutsPlus - Welcome to React Animations'}</h2>
+      </ReactCSSTransitionGroup>
+    </div>
+  );
+};
+
+render(
+  <Home />,
+  document.getElementById('app')
+);
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
